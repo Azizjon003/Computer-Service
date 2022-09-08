ba1f9094bee152ff979ae2249e4b467eb589246e
mamurjon7191 <ibragimovmamurjon19@gmail.com>
2022-09-08 18:31:30 +0500

    S

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import Button from "@mui/material/Button";
 
+import SignIn from "./pages/SignIn/SignIn";
+import SignUp from "./pages/SignUp/SignUp";
+
 const App = () => {
-  return <Button variant="contained">Hello World</Button>;
+  return (
+    <>
+      <Button variant="contained">Hello World</Button>
+      <SignIn />
+      <SignUp />
+    </>
+  );
 };
 
 export default App;
