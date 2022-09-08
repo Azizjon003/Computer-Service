d2e267895becb2949449db0ba441bf1e8f35644b
mamurjon7191 <ibragimovmamurjon19@gmail.com>
2022-09-08 19:07:17 +0500

    S

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,17 @@ import Button from "@mui/material/Button";
 import SignIn from "./pages/SignIn/SignIn";
 import SignUp from "./pages/SignUp/SignUp";
 import Checkout from "./pages/Checkout/Checkout";
+import Chart from "./pages/Dashboard/Chart";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 const App = () => {
   return (
     <>
-      <Button variant="contained">Hello World</Button>
+      {/* <Button variant="contained">Hello World</Button>
       <SignIn />
       <SignUp />
-      <Checkout />
+      <Checkout /> */}
+      <Dashboard />
     </>
   );
 };
