5349c832ada3bd71c6ad64d259e646beb7ab9739
Pulatoff <niyozbekpulatov@gmail.com>
2022-09-10 11:54:50 +0500

    loading screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,7 @@
 import React from "react";
-import Button from "@mui/material/Button";
-
 import SignIn from "./pages/SignIn/SignIn";
 import SignUp from "./pages/SignUp/SignUp";
 import Checkout from "./pages/Checkout/Checkout";
-import Chart from "./pages/Dashboard/Chart";
 import Dashboard from "./pages/Dashboard/Dashboard";
 
 const App = () => {
---
+++ client/src/routes/index.js
@@ -0,0 +1,5 @@
+import { Suspense, lazy } from "react";
+import { Navigate, useRoutes, useLocation } from "react-router-dom";
+// layouts
+
+//
