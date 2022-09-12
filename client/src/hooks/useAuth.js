b4609332e26ae675804e7eb6a7b77fc3186af0b5
Pulatoff <niyozbekpulatov@gmail.com>
2022-09-12 17:50:28 +0500

    auth context writed

---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+//
+import { AuthContext } from "../contexts/JWTContext";
+
+const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) throw new Error("Auth context must be inside AuthProvider");
+  return context;
+};
