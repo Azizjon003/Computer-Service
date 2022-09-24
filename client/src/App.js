4b96d6349f1650b81e49db69398a91763acb2a9d
sobit-1999 <109309119+sobit-1999@users.noreply.github.com>
2022-09-24 17:17:17 +0500

    Hali kamchiliklarini to'g'irliman

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
-import Routing from "./routes";
+import HeaderApp from "./components/headerApp/HeaderApp";
+import NavbarApp from "./components/headerApp/NavbarApp";
+import SidebarApp from "./components/sidebarApp/SidebarApp";
+import { categoryLink } from "./components/sidebarApp/SidebarData";
 import ThemeProvider from "./themes";
-
+console.log(categoryLink);
 const App = () => {
   return (
     <ThemeProvider>
-      <Routing />
+      <HeaderApp />
+      <NavbarApp />
     </ThemeProvider>
   );
 };
