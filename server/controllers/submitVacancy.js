0fdb2d0ef051647e3a51ff02bfd9dc69e532ea62
mamurjon7191 <ibragimovmamurjon19@gmail.com>
2022-09-24 17:57:21 +0500

    worker controller has started

---
+++ server/controllers/submitVacancy.js
@@ -0,0 +1,106 @@
+const SubmitVacancy = require("../models/submitVacancyModel");
+
+const User = require("../models/userModel");
+const Vacancy = require("../models/vacancyModel");
+const vacancyCategories = require("../models/vacancyCategoryModel");
+
+const getAll = async (req, res) => {
+  try {
+    const submitVacancy = await SubmitVacancy.findAll({
+      include: [
+        {
+          model: User,
+        },
+        {
+          model: Vacancy,
+          include: [
+            {
+              model: vacancyCategories,
+            },
+          ],
+        },
+      ],
+    });
+
+    res.status(200).json({
+      data: submitVacancy,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const add = async (req, res) => {
+  try {
+    const submitVacancy = await SubmitVacancy.create(req.body);
+    res.status(200).json({
+      data: submitVacancy,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const delete1 = async (req, res) => {
+  try {
+    await SubmitVacancy.destroy({ where: { id: req.params.id } });
+    res.status(200).json({
+      data: "success",
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+const getOne = async (req, res) => {
+  try {
+    const submitVacancy = await SubmitVacancy.findOne({
+      where: { id: req.params.id },
+      include: [
+        {
+          model: submitVacancy,
+        },
+        {
+          model: Vacancy,
+          include: [
+            {
+              model: vacancyCategories,
+            },
+          ],
+        },
+      ],
+    });
+    res.status(200).json({
+      data: submitVacancy,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+const update = async (req, res) => {
+  try {
+    const submitVacancy = await SubmitVacancy.findOne({
+      where: { id: req.params.id },
+    });
+
+    submitVacancy.status = req.body.status || submitVacancy.status;
+    submitVacancy.submitVacancyId =
+      req.body.submitVacancyId || submitVacancy.submitVacancyId;
+    submitVacancy.vacancyId = req.body.vacancyId || submitVacancy.vacancyId;
+
+    const newSubmitVacancy = await submitVacancy.save();
+
+    res.status(200).json({
+      data: newSubmitVacancy,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+module.exports = {
+  add,
+  getAll,
+  delete1,
+  getOne,
+  update,
+};
--- server/middlewares/app.js
+++ server/middlewares/app.js
@@ -7,10 +7,14 @@ const vacancyRouter = require("../routes/vacancyRouter");
 const locationRouter = require("../routes/locationRouter");
 const userRouter = require("../routes/userRouter");
 const vacancyCategories = require("../routes/vacancyCategoryRouter");
+const submitVacancy = require("../routes/submitVacancyRouter");
+const review = require("../routes/reviewRouter");
 
 app.use("/api/v1/vacancy", vacancyRouter);
 app.use("/api/v1/locations", locationRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/vacancyCategories", vacancyCategories);
+app.use("/api/v1/submitVacancy", submitVacancy);
+app.use("/api/v1/reviews", review);
 
 module.exports = app;
