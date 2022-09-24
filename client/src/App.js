4eabb42944d68145831d3d4a81bd2c7d0853b7d0
mamurjon7191 <ibragimovmamurjon19@gmail.com>
2022-09-24 18:27:23 +0500

    some bugs fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
+import Route from "./routes";
 import HeaderApp from "./components/headerApp/HeaderApp";
 import NavbarApp from "./components/headerApp/NavbarApp";
 import SidebarApp from "./components/sidebarApp/SidebarApp";
 import { categoryLink } from "./components/sidebarApp/SidebarData";
 import ThemeProvider from "./themes";
-console.log(categoryLink);
+// console.log(categoryLink);
 const App = () => {
   return (
     <ThemeProvider>
       <HeaderApp />
       <NavbarApp />
+      <Route />
     </ThemeProvider>
+    // <h1></h1>
   );
 };
 
